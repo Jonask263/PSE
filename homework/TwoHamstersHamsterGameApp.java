e47bb2c2da974903db18dc783308949db9a9e496
Jonask263 <149378401+Jonask263@users.noreply.github.com>
2023-11-21 20:04:25 +0100

    Delete homework directory

--- homework/TwoHamstersHamsterGameApp.java
+++
@@ -1,8 +0,0 @@
-package de.unistuttgart.iste.sqa.pse.sheet03.homework;
-
-public class TwoHamstersHamsterGameApp {
-	public static void main(String[] args) {
-		final TwoHamstersHamsterGame game = new TwoHamstersHamsterGame();
-		game.doRun();
-	}
-}
